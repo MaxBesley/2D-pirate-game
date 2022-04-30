3a06f80fdd544cb51b5c0dee22880015488122d2
Max Besley <mbesley@student.unimelb.edu.au>
2022-04-30 16:49:14 +1000

    initial commit

---
+++ src/ShadowPirate.java
@@ -0,0 +1,48 @@
+import bagel.*;
+
+/**
+ * Skeleton Code for SWEN20003 Project 2, Semester 1, 2022
+ *
+ * Please fill your name below
+ * @author
+ */
+public class ShadowPirate extends AbstractGame {
+    private final static int WINDOW_WIDTH = 1024;
+    private final static int WINDOW_HEIGHT = 768;
+    private final static String GAME_TITLE = "ShadowPirate";
+    private final Image BACKGROUND_IMAGE = new Image("res/background0.png");
+
+    public ShadowPirate() {
+        super(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE);
+    }
+
+    /**
+     * The entry point for the program.
+     */
+    public static void main(String[] args) {
+        ShadowPirate game = new ShadowPirate();
+        game.run();
+    }
+
+    /**
+     * Method used to read file and create objects
+     */
+    private void readCSV(String fileName){
+
+    }
+
+    /**
+     * Performs a state update.
+     * allows the game to exit when the escape key is pressed.
+     */
+    @Override
+    public void update(Input input) {
+        BACKGROUND_IMAGE.draw(Window.getWidth()/2.0, Window.getHeight()/2.0);
+
+        if (input.wasPressed(Keys.ESCAPE)){
+            Window.close();
+        }
+
+    }
+
+}
